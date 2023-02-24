eeda29052f03dea31f5ddd9a103c8b88126292d7
Brian Lee <brianlee1597@gmail.com>
2023-02-24 14:22:19 -0500

    almost done

--- speakextension/scripts/reader.js
+++ speakextension/scripts/reader.js
@@ -1,24 +1,39 @@
-$(document).ready(function () { 
-    $("*:not(body)").hover(
-        function (ev) { // when hover
-            $(this).addClass("highlight");
-            const text = $(this).text();
-            speechSynthesis.speak(new SpeechSynthesisUtterance(text)); 
-            ev.stopPropagation();
-        },
-        function (ev) { // when exit
-            $(this).removeClass("highlight");
-            speechSynthesis.cancel();
-            ev.stopPropagation(); 
-        }
-    )
-})
+jQuery(function () { 
+    let prev, curr;
 
-// $(document).keydown(function (e) {
-//     if (e.code == '' || e.code == 'Unidentified' || e.code == 'Space') {
-//         const text = $(this).text();
-//         speechSynthesis.speak(new SpeechSynthesisUtterance(text)); 
-//     } else {
-//         speechSynthesis.cancel();
-//     }
-// });
+    $(document).on('mousemove', function (e) {
+        curr = e.target;
+    })
+
+    $("*:not(body)").on("keydown", function (e) {
+        e.preventDefault();
+
+        if (e.key !== ' ') {
+            $(curr).removeClass("highlight");
+            (prev == curr) && speechSynthesis.cancel();
+            return;
+        }
+    
+        if (prev) {
+            $(prev).removeClass("highlight");
+            speechSynthesis.cancel();
+        }
+
+        let text;
+
+        if ($(curr).is('img')) {
+            const alttext = $(curr).attr("alt");
+            const srcofimg = $(curr).attr("src");
+
+            text = $(curr).attr('alt') ? alttext : srcofimg;
+        } else {
+            text = $(curr).text();
+        }
+
+        $(curr).addClass("highlight");
+        const speech = new SpeechSynthesisUtterance(text);
+        speechSynthesis.speak(speech); 
+
+        prev = curr;
+    })
+})
